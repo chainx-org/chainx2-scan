9f8482abc34347c427644e96c514f9ff8a31216c
Yongfeng LI <wliyongfeng@gmail.com>
2020-07-13 18:06:28 +0800

    Introduce index.scss

--- packages/site/src/index.js
+++ packages/site/src/index.js
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import GlobalStyle from "./GlobalStyle";
+import "./index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
