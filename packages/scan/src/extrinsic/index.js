a96d7ce413046e7b278bb5817b643ccd71e56e70
Yongfeng LI <wliyongfeng@gmail.com>
2020-09-03 16:58:46 +0800

    Add sections to ignore custom handle

--- packages/scan/src/extrinsic/index.js
+++ packages/scan/src/extrinsic/index.js
@@ -1,12 +1,17 @@
 const { extractUserTransfer, updateBalance } = require('../account')
+const ignoreSectionNames = ['timestsamp']
 
 async function extractExtrinsicBusinessData(extrinsic, indexer) {
-  const section = extrinsic.method.sectionName
-  const methodName = extrinsic.method.methodName
+  const section = extrinsic.method.sectionName.toLowerCase()
+  if (ignoreSectionNames.includes(section)) {
+    return
+  }
+
+  const methodName = extrinsic.method.methodName.toLowerCase()
   const { args } = extrinsic.method.toJSON()
   const signer = extrinsic._raw.signature.get('signer').toString()
 
-  if (section.toLowerCase() === 'balances' && methodName === 'transfer') {
+  if (section === 'balances' && methodName === 'transfer') {
     await updateBalance(extrinsic, signer, args.dest)
     await extractUserTransfer(extrinsic, indexer, signer, args)
   }
