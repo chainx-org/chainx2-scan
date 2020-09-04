a92b8814f9b24aaccf71e48b9376043583215be4
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-09-04 23:10:31 +0800

    .

--- packages/scan/src/api/index.js
+++ packages/scan/src/api/index.js
@@ -1,17 +1,16 @@
 const { ApiPromise, WsProvider } = require('@chainx-v2/api')
+const { logger } = require('../util')
 
 let provider = null
 let api = null
 
 async function getApi() {
   if (!api) {
-    let ws_endpoint
-    if (process.env.WS_ENDPOINT) {
-      ws_endpoint = process.env.WS_ENDPOINT
-    } else {
-      ws_endpoint = 'ws://47.114.131.193:9000'
-    }
+    const ws_endpoint = process.env.WS_ENDPOINT
+      ? process.env.WS_ENDPOINT
+      : 'ws://47.114.131.193:9000'
     provider = new WsProvider(ws_endpoint)
+    logger.info('Connecting to Node at ', ws_endpoint)
     api = await ApiPromise.create({ provider })
   }
 
