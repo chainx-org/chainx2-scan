4735f9eeee97bff6d3da03b124ef237664f1b911
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-09-06 22:14:54 +0800

    Handle Unbond and Rebond

--- packages/scan/src/events/index.js
+++ packages/scan/src/events/index.js
@@ -38,30 +38,52 @@ function getCurrentVote(col, nominator, nominee) {
   })
 }
 
+async function insertNewVote(col, nominator, nominee, value) {
+  await col.findOneAndUpdate(
+    {
+      nominator: nominator,
+      nominee: nominee
+    },
+    {
+      $set: {
+        nominator: nominator,
+        nominee: nominee,
+        value: value
+      }
+    },
+    { upsert: true }
+  )
+}
+
 async function handleStakingEvents(method, event) {
   if (method === 'Bond') {
     let [nominator, nominee, value] = event.data.toJSON()
     value = parseInt(value)
-
     const col = await getVoteCollection()
     const result = await getCurrentVote(col, nominator, nominee)
-
     const new_value = result.length ? value + result[0].value : value
+    await insertNewVote(col, nominator, nominee, new_value)
+  } else if (method === 'Unbond') {
+    let [nominator, nominee, value] = event.data.toJSON()
+    value = parseInt(value)
+    const col = await getVoteCollection()
+    const result = await getCurrentVote(col, nominator, nominee)
+    /** result.length > 0, qed */
+    const new_value = result[0].value - value
+    await insertNewVote(col, nominator, nominee, new_value)
+  } else if (method === 'Rebond') {
+    let [nominator, from, to, value] = event.data.toJSON()
+    value = parseInt(value)
+    const col = await getVoteCollection()
 
-    await col.findOneAndUpdate(
-      {
-        nominator: nominator,
-        nominee: nominee
-      },
-      {
-        $set: {
-          nominator: nominator,
-          nominee: nominee,
-          value: new_value
-        }
-      },
-      { upsert: true }
-    )
+    const from_result = await getCurrentVote(col, nominator, from)
+    const new_from_value = from_result[0].value - value
+
+    const to_result = await getCurrentVote(col, nominator, to)
+    const new_to_value = to_result.length ? value + to_result[0].value : value
+
+    await insertNewVote(col, nominator, from, new_from_value)
+    await insertNewVote(col, nominator, to, new_to_value)
   }
 }
 
