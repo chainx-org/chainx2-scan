e549aa042d3dff0c39d5dbdfcad210862dc11171
qinghuan <hashlock@163.com>
2020-09-08 10:48:08 +0800

    remove account update

--- packages/scan/src/account/updateBalance.js
+++
@@ -1,9 +0,0 @@
-const { getAccountsCollection, getEventCollection } = require('../mongoClient')
-const { getPCXAssetByAccount, getAllAssetByAccount } = require('./common')
-
-module.exports = async function extractAuthor(extrinsic, hash, from, dest) {
-  if (!from) {
-    return
-  }
-  // TODO
-}
