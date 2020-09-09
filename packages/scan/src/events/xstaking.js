0d25a1ebd8c0f26b90b3ecd8407e2b34e93c2a53
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-09-09 12:51:46 +0800

    Fetch the whole nomination list from RPC

--- packages/scan/src/events/xstaking.js
+++ packages/scan/src/events/xstaking.js
@@ -1,36 +1,16 @@
 const { getVoteCollection } = require('../mongoClient')
-const BigNumber = require('bignumber.js')
 const { getApi } = require('../api')
-const _ = require('lodash')
 const { logger } = require('../util')
 
-const emptyNominatorLedger = {
-  nomination: '0',
-  lastVoteWeight: '0',
-  lastVoteWeightUpdate: '0'
-}
+const safeBlocks = 300
 
-async function getNominatorLedger(blockHash, nominator, nominee) {
+async function getNominationsAt(nominator, blockHash) {
   const api = await getApi()
-  let nominatorLedger = await api.query.xStaking.nominations.at(
-    blockHash,
+  const nominations = await api.rpc.xstaking.getNominationByAccount(
     nominator,
-    nominee
+    blockHash
   )
-
-  if (_.isEmpty(nominatorLedger)) {
-    nominatorLedger = emptyNominatorLedger
-  }
-
-  return Object.entries(nominatorLedger).reduce((result, [key, value]) => {
-    /** TODO: also handle unbondedChunks? */
-    if (
-      ['nomination', 'lastVoteWeight', 'lastVoteWeightUpdate'].includes(key)
-    ) {
-      result[key] = new BigNumber(value).toString()
-    }
-    return result
-  }, {})
+  return nominations
 }
 
 async function insertNewNominations(col, blockHeight, nominator, nominations) {
@@ -50,83 +30,31 @@ async function insertNewNominations(col, blockHeight, nominator, nominations) {
   )
 }
 
-const historyDepth = 10
-
 async function updateNominatorLedgerAt(
   blockHeight,
   blockHash,
   nominator,
   nominee
 ) {
-  const nominatorLedger = await getNominatorLedger(
-    blockHash,
-    nominator,
-    nominee
-  )
-
+  const nominations = await getNominationsAt(nominator, blockHash)
   const col = await getVoteCollection()
+  await col.insertOne({
+    blockHeight,
+    nominator,
+    nominations: nominations.toJSON()
+  })
 
-  const result = await col
+  const records = await col
     .find({
-      nominator
+      nominator,
+      blockHeight: { $lt: blockHeight - safeBlocks }
     })
     .toArray()
 
-  if (!result.length) {
-    await col.insertOne({
-      blockHeight,
-      nominator,
-      nominations: [{ nominee, nominatorLedger }]
-    })
-  } else {
-    const maxHeight = Math.max(...result.map(r => r.blockHeight))
-    const bestResult = await col
-      .find({
-        blockHeight: maxHeight,
-        nominator
-      })
-      .limit(1)
-      .toArray()
-
-    const bestState = bestResult[0]
-
-    /** Remove the existing old nomination of nominee and then push the new one. */
-    let newNominations = _.filter(bestState.nominations, function(el) {
-      return el.nominee !== nominee
-    })
-    newNominations.push({
-      nominee,
-      nominatorLedger
-    })
-
-    await insertNewNominations(col, blockHeight, nominator, newNominations)
-
-    /** TODO: optimize the pruning, take finalizedHeight into account. */
-    if (blockHeight - historyDepth > 0 && result.length > 1) {
-      logger.debug(
-        `[vote]pruning the state of ${nominator} older than height ${blockHeight -
-          historyDepth}, current history size: ${result.length}`
-      )
-      // TODO: improve the pruning
-      // This deletion can delete all the old state except the new inserted one,
-      // so we check the pruned result again and keep the bestState when there is
-      // no other history state.
-      await col.deleteMany({
-        blockHeight: { $lt: blockHeight - historyDepth },
-        nominator
-      })
-      const pruned_result = await col
-        .find({
-          nominator
-        })
-        .toArray()
-      if (pruned_result.length <= 1) {
-        logger.debug(
-          `[vote]restore the bestState of ${nominator} at height ${maxHeight}`
-        )
-        await col.insertOne(bestState)
-      }
-    }
+  if (records.length > 1) {
+    const maxSafeHeight = Math.max(...records.map(r => r.blockHeight))
+    logger.info(`[vote]pruning the old state before height ${maxSafeHeight}`)
+    col.deleteMany({ blockHeight: { $lt: maxSafeHeight } })
   }
 }
 
