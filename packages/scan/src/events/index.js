cd0ed2054b1edb13339ef926f98e6c961197e5ae
qinghuan <hashlock@163.com>
2020-09-09 13:34:34 +0800

    merge

--- packages/scan/src/events/index.js
+++ packages/scan/src/events/index.js
@@ -12,7 +12,7 @@ async function extractEventBusinessData(event, indexer) {
   } else if (section === 'balances') {
     await handleBalancesEvent(event, indexer)
   } else if (section === 'xSpot') {
-    await handleSpotEvent(method, event)
+    await handleSpotEvent(event, indexer)
   } else if (section === 'xStaking') {
     await handleStakingEvent(method, event)
   }
--- packages/scan/src/events/xspot.js
+++ packages/scan/src/events/xspot.js
@@ -1,24 +1,59 @@
 const { getOrdersCollection } = require('../mongoClient')
+const { getApi } = require('../api')
 
-function getNormalizedOrderFromEvent(event) {
-  const order = event.data.toJSON()[0]
-  order.props.amount = parseInt(order.props.amount)
-  return order
+const safeBlocks = 300
+
+async function getOrdersList(submitter, blockHash) {
+  const api = await getApi()
+  console.log('submitter:' + submitter)
+  const orders = await api.rpc.xspot.getOrdersByAccount(submitter, 0, 100)
+  return orders
 }
 
-async function handleSpotEvent(method, event) {
-  /** FIXME: Update to the latest Spot Event */
-  if (method === 'PutOrder') {
-    const order = getNormalizedOrderFromEvent(event)
-    const col = await getOrdersCollection()
-    col.insertOne(order)
-  } else if (method === 'UpdateOrder') {
-    const order = getNormalizedOrderFromEvent(event)
-    const col = await getOrdersCollection()
-    await col.findOneAndReplace(
-      { 'props.id': order.props.id, 'props.submitter': order.props.submitter },
-      order,
-      { upsert: true }
-    )
+async function updateOrdersAt(blockHeight, blockHash, submitter) {
+  const orders = await getOrdersList(submitter, blockHash)
+  const col = await getOrdersCollection()
+  await col.insertOne({
+    blockHeight,
+    submitter,
+    orders: orders.toJSON()
+  })
+
+  const records = await col
+    .find({
+      submitter,
+      blockHeight: { $lt: blockHeight - safeBlocks }
+    })
+    .toArray()
+
+  if (records.length > 1) {
+    const maxSafeHeight = Math.max(...records.map(r => r.blockHeight))
+    logger.info(`[vote]pruning the old state before height ${maxSafeHeight}`)
+    col.deleteMany({ blockHeight: { $lt: maxSafeHeight } })
   }
+}
+
+async function handleSpotEvent(event, indexer) {
+  const { method } = event
+  const { blockHeight, blockHash } = indexer
+  // create new order
+  if (
+    [
+      'NewOrder',
+      'MakerOrderUpdated',
+      'TakerOrderUpdated',
+      'OrderExecuted',
+      'CanceledOrderUpdated'
+    ].includes(method)
+  ) {
+    console.log(event.data.toJSON().pop())
+    let {
+      props: { submitter }
+    } = event.data.toJSON().pop()
+    await updateOrdersAt(blockHeight, blockHash, submitter)
+  }
+}
+
+module.exports = {
+  handleSpotEvent
 }
