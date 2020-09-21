bb1935b388f2d8de3ee5a8337a60ed6933235517
Yongfeng LI <wliyongfeng@gmail.com>
2020-09-21 11:34:43 +0800

    Add dex fills api

---
+++ packages/server/src/features/dex/fill.controller.js
@@ -0,0 +1,35 @@
+const { getDb } = require('../../services/mongo')
+const { extractPage } = require('../../utils')
+
+class FillController {
+  async getFills(ctx) {
+    const { page, pageSize } = extractPage(ctx)
+    if (pageSize === 0) {
+      ctx.status = 400
+      return
+    }
+
+    const { pairId } = ctx.params
+
+    const db = await getDb()
+    const col = await db.collection('deals')
+
+    const query = { pairId: parseInt(pairId) }
+    const total = await col.countDocuments(query)
+    const items = await col
+      .find(query)
+      .sort({ queryHeight: -1 })
+      .skip(page * pageSize)
+      .limit(pageSize)
+      .toArray()
+
+    ctx.body = {
+      items,
+      page,
+      pageSize,
+      total
+    }
+  }
+}
+
+module.exports = new FillController()
--- packages/server/src/features/dex/routes.js
+++ packages/server/src/features/dex/routes.js
@@ -1,9 +1,11 @@
 const Router = require('koa-router')
 const dexController = require('./dex.controller')
 const orderController = require('./order.controller')
+const fillController = require('./fill.controller')
 
 const router = new Router()
 router.get('/dex/pairs', dexController.getPairs)
 router.get('/dex/orders', orderController.getOpenOrders)
+router.get('/dex/fills/:pairId', fillController.getFills)
 
 module.exports = router
