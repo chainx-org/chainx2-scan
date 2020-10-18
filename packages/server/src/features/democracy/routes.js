1ec2ef41213bb74dd7346381b55b8b0f0b28dd83
qinghuan-deep <1245816264@qq.com>
2020-10-18 15:19:49 +0800

    fix routes

--- packages/server/src/features/democracy/routes.js
+++ packages/server/src/features/democracy/routes.js
@@ -2,7 +2,7 @@ const Router = require('koa-router')
 const democracyController = require('./democracy.controller')
 
 const router = new Router()
-router.post('/submit_democracy', democracyController.submitDemocracy)
-router.get('/get_democracy', democracyController.getDemocracy)
+router.post('/democracy/submit_democracy', democracyController.submitDemocracy)
+router.get('/democracy/get_democracy', democracyController.getDemocracy)
 
 module.exports = router
