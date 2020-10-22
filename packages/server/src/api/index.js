ed0fd1c0e185d94cd2a576c73a02dd0dd4d873a3
bing <tarvos21@gmail.com>
2020-10-22 18:13:13 +0800

    Update websocket server IP address in server

--- packages/server/src/api/index.js
+++ packages/server/src/api/index.js
@@ -8,7 +8,8 @@ async function getApi() {
   if (!api) {
     const ws_endpoint = process.env.WS_ENDPOINT
       ? process.env.WS_ENDPOINT
-      : 'ws://47.114.131.193:9000'
+      : // : 'ws://47.114.131.193:9000'
+        'ws://47.99.209.200:9000'
     provider = new WsProvider(ws_endpoint)
     api = await ApiPromise.create(options({ provider }))
   }
