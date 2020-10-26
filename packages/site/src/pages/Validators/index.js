4af2e70225b341d9a949c20dab54d5117b31ca54
bing <tarvos21@gmail.com>
2020-10-26 18:38:09 +0800

    Add validators list ( isValidating = true )

--- packages/site/src/pages/Validators/index.js
+++ packages/site/src/pages/Validators/index.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router'
 
 import BlockDetail from './Detail'
 import BlocksList from './List'
+import ValidatorNodesList from './validatorNodes'
 import ValidatorsNav from '../../components/ValidatorsNav'
 
 export default function Validators() {
@@ -20,7 +21,9 @@ export default function Validators() {
             <>
               <div className="box">
                 <ValidatorsNav activeKey={list} />
-                {(!list || list === 'validators') && <BlocksList {...props} />}
+                {(!list || list === 'validators') && (
+                  <ValidatorNodesList {...props} />
+                )}
                 {list === 'unsettled' && <BlocksList {...props} />}
                 {list === 'btc_trustees' && <BlocksList {...props} />}
                 {list === 'missed' && <BlocksList {...props} />}
