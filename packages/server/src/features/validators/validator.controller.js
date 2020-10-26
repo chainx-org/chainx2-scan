3e8f595c6b1d4b64882765479c9f3240fff594b8
bing <tarvos21@gmail.com>
2020-10-26 18:41:15 +0800

    Add API to serve validators list data

---
+++ packages/server/src/features/validators/validator.controller.js
@@ -0,0 +1,205 @@
+const { getDb } = require('../../services/mongo')
+const { extractPage } = require('../../utils')
+
+class validatorsController {
+  async getValidators(ctx) {
+    const { page, pageSize } = extractPage(ctx)
+    if (pageSize === 0) {
+      ctx.status = 400
+      return
+    }
+
+    const db = await getDb()
+    const col = await db.collection('validators')
+
+    const query = { isValidating: true }
+
+    const total = await col.countDocuments(query)
+    const items = await col
+      .find(query)
+      .sort({ lastTotalVoteWeight: -1 })
+      .collation({ locale: 'en_US', numericOrdering: true })
+      .skip(page * pageSize)
+      .limit(pageSize)
+      .toArray()
+
+    ctx.body = {
+      items,
+      page,
+      pageSize,
+      total
+    }
+  }
+
+  /*
+  async getCrossTransactions(ctx) {
+    const { page, pageSize } = extractPage(ctx)
+    if (pageSize === 0) {
+      ctx.status = 400
+      return
+    }
+
+    const db = await getDb()
+    const col = await db.collection('crossTransaction')
+
+    const query = {}
+
+    const total = await col.countDocuments(query)
+    const items = await col
+      .find(query)
+      .sort({ btcHeight: -1 })
+      .skip(page * pageSize)
+      .limit(pageSize)
+      .toArray()
+
+    ctx.body = {
+      items,
+      page,
+      pageSize,
+      total
+    }
+  }
+
+  async getCrossDeposits(ctx) {
+    const { page, pageSize } = extractPage(ctx)
+    if (pageSize === 0) {
+      ctx.status = 400
+      return
+    }
+
+    const db = await getDb()
+    const col = await db.collection('crossTransaction')
+
+    const query = { txType: 'Deposit' }
+
+    const total = await col.countDocuments(query)
+    const items = await col
+      .find(query)
+      .sort({ btcHeight: -1 })
+      .skip(page * pageSize)
+      .limit(pageSize)
+      .toArray()
+
+    ctx.body = {
+      items,
+      page,
+      pageSize,
+      total
+    }
+  }
+
+  async getCrossWithdrawals(ctx) {
+    const { page, pageSize } = extractPage(ctx)
+    if (pageSize === 0) {
+      ctx.status = 400
+      return
+    }
+
+    const db = await getDb()
+    const col = await db.collection('crossTransaction')
+
+    const query = { txType: 'Deposit' }
+
+    const total = await col.countDocuments(query)
+    const items = await col
+      .find(query)
+      .sort({ btcHeight: -1 })
+      .skip(page * pageSize)
+      .limit(pageSize)
+      .toArray()
+
+    ctx.body = {
+      items,
+      page,
+      pageSize,
+      total
+    }
+  }
+
+  async getCrossTrustees(ctx) {
+    const { page, pageSize } = extractPage(ctx)
+    if (pageSize === 0) {
+      ctx.status = 400
+      return
+    }
+
+    const db = await getDb()
+    const col = await db.collection('trustees')
+
+    const query = {}
+
+    const total = await col.countDocuments(query)
+    const items = await col
+      .find(query)
+      .sort({ threshold: -1 })
+      .skip(page * pageSize)
+      .limit(pageSize)
+      .toArray()
+
+    ctx.body = {
+      items,
+      page,
+      pageSize,
+      total
+    }
+  }
+
+  async getCrossUnclaim(ctx) {
+    const { page, pageSize } = extractPage(ctx)
+    if (pageSize === 0) {
+      ctx.status = 400
+      return
+    }
+
+    const db = await getDb()
+    const col = await db.collection('crossTransaction')
+
+    const query = { txType: 'Deposit' }
+
+    const total = await col.countDocuments(query)
+    const items = await col
+      .find(query)
+      .sort({ btcHeight: -1 })
+      .skip(page * pageSize)
+      .limit(pageSize)
+      .toArray()
+
+    ctx.body = {
+      items,
+      page,
+      pageSize,
+      total
+    }
+  }
+
+  async getDepositMine(ctx) {
+    const { page, pageSize } = extractPage(ctx)
+    if (pageSize === 0) {
+      ctx.status = 400
+      return
+    }
+
+    const db = await getDb()
+    const col = await db.collection('depositMine')
+
+    const query = {}
+
+    const total = await col.countDocuments(query)
+    const items = await col
+      .find(query)
+      .sort({ assetId: -1 })
+      .skip(page * pageSize)
+      .limit(pageSize)
+      .toArray()
+
+    ctx.body = {
+      items,
+      page,
+      pageSize,
+      total
+    }
+  }
+  */
+}
+
+module.exports = new validatorsController()
