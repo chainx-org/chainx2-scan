d79977e6e8b93e29ba18878d918df3ec3da1d3f7
limiu331 <845217637@qq.com>
2020-11-02 16:45:08 +0800

    add bitcoinAddress router

--- packages/server/src/features/homepage/router.js
+++ packages/server/src/features/homepage/router.js
@@ -3,4 +3,5 @@ const homeController = require('./home.controller')
 const router = new Router()
 router.get('/home/bitcoinbridge', homeController.getBtcCoinBridgeDeposited)
 router.get('/home/bitcoinwithdrawl', homeController.getBtcCoinBridgeWithdrawl)
+router.get('/home/bitcoinAddress', homeController.getBtcAddress)
 module.exports = router
