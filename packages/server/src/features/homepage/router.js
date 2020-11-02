ff34a22e5eecebd2b9824e5ea9fa5ea2a98a2792
limiu331 <845217637@qq.com>
2020-11-02 11:54:25 +0800

    add btc withdrawl router

--- packages/server/src/features/homepage/router.js
+++ packages/server/src/features/homepage/router.js
@@ -2,4 +2,5 @@ const Router = require('koa-router')
 const homeController = require('./home.controller')
 const router = new Router()
 router.get('/home/bitcoinbridge', homeController.getBtcCoinBridgeDeposited)
+router.get('/home/bitcoinwithdrawl', homeController.getBtcCoinBridgeWithdrawl)
 module.exports = router
