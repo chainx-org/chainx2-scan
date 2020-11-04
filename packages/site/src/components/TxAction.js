6740cc0e68479c16c1f19d22436860154830d15c
limiu331 <845217637@qq.com>
2020-11-04 16:47:17 +0800

    add translate

--- packages/site/src/components/TxAction.js
+++ packages/site/src/components/TxAction.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import classnames from 'classnames'
-
+import $t from '../locale'
 export default function TxAction(props) {
   const { module, call, className, style } = props
 
   return (
-    <div className={classnames(className)} style={style}>{`${module}${
+    <div className={classnames(className)} style={style}>{`${$t(module)}${
       call ? `(${call})` : ''
     }`}</div>
   )
