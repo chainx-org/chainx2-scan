c981a8cfcbdcd1ff6bfab698eadfb34c0cc53d74
limiu331 <845217637@qq.com>
2020-11-06 18:22:30 +0800

    add otherAsset translate

--- packages/site/src/locale/messages/assets.js
+++ packages/site/src/locale/messages/assets.js
@@ -3,10 +3,26 @@ export default {
     zh: 'ChainX资产',
     en: 'ChainX Assets'
   },
+  CROSS_ASSET: {
+    zh: '跨链资产',
+    en: 'Cross-chain Assets'
+  },
 
   ASSETNAME: {
     zh: '资产',
     en: 'Asset'
+  },
+  OriginChain: {
+    zh: '原链',
+    en: 'Origin Chain'
+  },
+  ReservedBalance: {
+    zh: '保留余额',
+    en: 'Reserved Balance'
+  },
+  withdrawalFreeze: {
+    zh: '提现冻结',
+    en: 'Cash withdrawal freeze'
   },
   FREEBALANCE: {
     zh: '可用余额',
@@ -15,6 +31,10 @@ export default {
   STAKINGRESERVED: {
     zh: '投票冻结',
     en: 'Staking Reserved'
+  },
+  LOCKEDBALANCE: {
+    zh: '锁定余额',
+    en: 'Locked Balance'
   },
   UNFREEZERESERVED: {
     zh: '赎回冻结',
