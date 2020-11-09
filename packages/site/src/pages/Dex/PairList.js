9ea3bb492fbdbc33b9d3ddaf08159d8854da382c
limiu331 <845217637@qq.com>
2020-11-09 18:36:50 +0800

    add SDOT assets

--- packages/site/src/pages/Dex/PairList.js
+++ packages/site/src/pages/Dex/PairList.js
@@ -3,14 +3,15 @@ import $t from '@src/locale'
 import { useSelector } from 'react-redux'
 import { pairsSelector } from '@src/store/reducers/dexSlice'
 import PCX from '../../assets/tokens/pcx_circle.jpg'
-
+import SDOT from '../../assets/tokens/sdot.jpg'
 import classnames from 'classnames'
 import TokenName from '@src/pages/Dex/TokenName'
 
 const tokenImgs = [PCX]
-
+const SDOTImgs = [SDOT]
 export default function PairList() {
   const pairs = useSelector(pairsSelector)
+  console.log(pairs)
 
   return (
     <section className="panel">
@@ -24,17 +25,31 @@ export default function PairList() {
               currencyPair: { base, quote }
             } = pair
             const currencyImg = tokenImgs[base]
-
+            const SDOTImg = SDOTImgs[base]
+            console.log(base, 'base')
+            console.log(tokenImgs, 'token')
             return (
-              <div key={idx} className={classnames('pairs-item', 'active')}>
-                {currencyImg && (
-                  <img
-                    src={currencyImg}
-                    className="pairs-item-icon"
-                    alt={base}
-                  />
-                )}
-                <TokenName id={base} />/<TokenName id={quote} />
+              <div>
+                <div key={idx} className={classnames('pairs-item', 'active')}>
+                  {currencyImg && (
+                    <img
+                      src={currencyImg}
+                      className="pairs-item-icon"
+                      alt={base}
+                    />
+                  )}
+                  <TokenName id={base} />
+                  /
+                  <TokenName id={quote} />
+                </div>
+                <div key={idx} className={classnames('pairs-item', 'active')}>
+                  {SDOTImg && (
+                    <img src={SDOTImg} className="pairs-item-icon" alt={base} />
+                  )}
+                  <TokenName id={base} />
+                  /
+                  <TokenName id={quote} />
+                </div>
               </div>
             )
           })}
