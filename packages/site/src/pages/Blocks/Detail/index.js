a1e71e698612124b16ea32591b889287a1a57573
limiu331 <845217637@qq.com>
2020-11-12 10:10:05 +0800

    blocks detail more friendly

--- packages/site/src/pages/Blocks/Detail/index.js
+++ packages/site/src/pages/Blocks/Detail/index.js
@@ -16,7 +16,6 @@ import NoData from '../../../components/NoData'
 import Paneljson from '../../../components/PanelJson'
 
 export default function() {
-
   const { heightOrHash } = useParams()
 
   const params = useMemo(() => [heightOrHash], [heightOrHash])
@@ -26,6 +25,38 @@ export default function() {
   const latestHeight = useSelector(latestHeightSelector)
   const hasNext = block?.header?.number < latestHeight
   const preHeight = block?.header?.number - 1
+  const deviceWidth = document.documentElement.clientWidth
+
+  const header =
+    deviceWidth < 1024 ? null : (
+      <div className="switch-block">
+        <NavLink
+          to={`/blocks/${preHeight < 0 ? block?.header?.number : preHeight}`}
+        >
+          <i
+            className={classnames(
+              { forbidden: preHeight < 0 },
+              'iconfont icon-left'
+            )}
+          />
+        </NavLink>
+        {$t('block_height')}:{block?.header?.number}
+        <NavLink
+          to={
+            hasNext
+              ? `/blocks/${block?.header?.number + 1}`
+              : `/blocks/${block?.header?.number}`
+          }
+        >
+          <i
+            className={classnames(
+              { forbidden: !hasNext },
+              'iconfont icon-right'
+            )}
+          />
+        </NavLink>
+      </div>
+    )
 
   const breadcrumb = (
     <Breadcrumb
@@ -54,33 +85,7 @@ export default function() {
   return (
     <div>
       {breadcrumb}
-      <div className="switch-block">
-        <NavLink
-          to={`/blocks/${preHeight < 0 ? block?.header?.number : preHeight}`}
-        >
-          <i
-            className={classnames(
-              { forbidden: preHeight < 0 },
-              'iconfont icon-left'
-            )}
-          />
-        </NavLink>
-        {$t('block_height')}:{block?.header?.number}
-        <NavLink
-          to={
-            hasNext
-              ? `/blocks/${block?.header?.number + 1}`
-              : `/blocks/${block?.header?.number}`
-          }
-        >
-          <i
-            className={classnames(
-              { forbidden: !hasNext },
-              'iconfont icon-right'
-            )}
-          />
-        </NavLink>
-      </div>
+      {header}
       <PanelList
         dataSource={[
           {
@@ -109,10 +114,10 @@ export default function() {
               <DateShow value={block?.blockTime} format="YYYY-MM-DD HH:mm:ss" />
             )
           },
-           {
+          {
             label: $t('block_digest'),
             data: <Paneljson json={block?.header?.digest} />
-          },
+          }
         ]}
       />
       <Extrinsics blockHeight={block?.header?.number} />
