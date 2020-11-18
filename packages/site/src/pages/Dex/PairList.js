288ee219a396ffbc26986169598618128a62bb69
limiu331 <845217637@qq.com>
2020-11-18 14:24:38 +0800

    Add  Trading Pair data display

--- packages/site/src/pages/Dex/PairList.js
+++ packages/site/src/pages/Dex/PairList.js
@@ -13,15 +13,14 @@ import {
   tradingPairsSelector
 } from '../../store/reducers/dexSlice'
 import { openOrdersSelector } from '../../store/reducers/accountSlice'
+import Amount from '../../components/Amount'
 
-const tokenImgs = [PCX]
 export default function PairList() {
   const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(fetchFills())
+    dispatch(fetchTradingPairs())
   }, [dispatch])
-  const pairs = useSelector(fillsSelector)
-  console.log(pairs, 'dsad')
+  const Trandingpairs = useSelector(tradingPairsSelector)
   return (
     <section className="panel">
       <div className="panel-heading" style={{ border: '1px solid #dbdbdb' }}>
@@ -29,28 +28,42 @@ export default function PairList() {
       </div>
       <div className="panel-block pairs" style={{ minHeight: 365 }}>
         <div className="pairs-items">
-          {pairs.map((pair, idx) => {
-            const {
-              currencyPair: { base, quote }
-            } = pair
-            const currencyImg = tokenImgs[base]
-            return (
-              <div>
-                <div key={idx} className={classnames('pairs-item', 'active')}>
-                  {currencyImg && (
-                    <img
-                      src={currencyImg}
-                      className="pairs-item-icon"
-                      alt={base}
-                    />
-                  )}
-                  <TokenName id={base} />
-                  /
-                  <TokenName id={quote} />
-                </div>
-              </div>
-            )
-          })}
+          <div>
+            <div className={classnames('pairs-item', 'active')}>
+              <img src={PCX} className="pairs-item-icon" />
+              <div>PCX/BTC</div>
+            </div>
+          </div>
+        </div>
+        <div className={'pairs-content'}>
+          <div className={'pairs-content-item'}>
+            <div className="pairs-content-item__label">
+              {$t('dex_latest_deal')}
+            </div>
+            <div className="pairs-content-item__value" style={{ fontSize: 24 }}>
+              <Amount
+                value={Trandingpairs.latestTransactionPrices}
+                precision={7}
+                symbol={'BTC'}
+              />
+            </div>
+          </div>
+          <div className={'pairs-content-item'}>
+            <div className="pairs-content-item__label">
+              {$t('dex_day_deal')}
+            </div>
+            <div className="pairs-content-item__value" style={{ fontSize: 24 }}>
+              <div>{Trandingpairs.TransactionsDayNumber}</div>
+            </div>
+          </div>
+          <div className={'pairs-content-item'}>
+            <div className="pairs-content-item__label">
+              {$t('dex_week_deal')}
+            </div>
+            <div className="pairs-content-item__value" style={{ fontSize: 24 }}>
+              <div>{Trandingpairs.TransactionsWeekNumber}</div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
