afa9b0f6d1931c02e33b5d99331ba20c11f56eba
limiu331 <845217637@qq.com>
2020-11-23 18:11:05 +0800

    add referralID search

--- packages/server/src/features/validators/validator.controller.js
+++ packages/server/src/features/validators/validator.controller.js
@@ -109,6 +109,60 @@ class validatorsController {
       total
     }
   }
+
+  async getMissed(ctx) {
+    const { page, pageSize } = extractPage(ctx)
+    if (pageSize === 0) {
+      ctx.status = 400
+      return
+    }
+
+    const db = await getDb()
+    const col = await db.collection('event')
+
+    const api = await getApi()
+
+    const query = { section: 'xStaking', method: 'Slashed' }
+    const addressInfo = await col
+      .find(query)
+      .sort({ totalNomination: -1 })
+      .collation({ locale: 'en_US', numericOrdering: true })
+      .skip(page * pageSize)
+      .limit(pageSize)
+      .toArray()
+    const address = addressInfo.map(item => item.data[0])
+    function unique(arr) {
+      return Array.from(new Set(arr))
+    }
+    let unitAddress = unique(address)
+    const item = unitAddress.reduce((obj, name) => {
+      if (name in obj) {
+        obj[name]++
+      } else {
+        obj[name] = 1
+      }
+      return obj
+    }, {})
+    const vacol = await db.collection('validators')
+    let items = []
+    let total = 0
+    for (let i = 0; i < unitAddress.length; i++) {
+      let unit = unitAddress[i]
+      let unitquery = { account: unit }
+      total = await vacol.countDocuments(unitquery)
+      let nickname = await vacol.find(unitquery).toArray()
+      let accountitem = nickname[0].account
+      nickname[0].missed = item[accountitem]
+      items.push(...nickname)
+    }
+
+    ctx.body = {
+      items,
+      page,
+      pageSize,
+      total
+    }
+  }
 }
 
 module.exports = new validatorsController()
