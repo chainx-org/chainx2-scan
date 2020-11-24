41171ae1e20ee2814e4e3d8e6c2bf2edfe5c61d1
bing <tarvos21@gmail.com>
2020-11-24 17:24:23 +0800

    Fix validator link component

--- packages/site/src/components/ValidatorLink.js
+++ packages/site/src/components/ValidatorLink.js
@@ -1,34 +1,26 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import classnames from 'classnames'
+import { Empty } from 'antd'
 
 import hexAddPrefix from '@polkadot/util/hex/addPrefix'
 import { FormattedMessage } from 'react-intl'
 
-export default function VilidatorLink({
-  value,
-  name = false,
-  isActive = true,
-  style,
-  index = '',
-  className,
-  filter = 'detail'
-}) {
-  const hexValue = value
-
-  if (name === null) {
-    name = ''
-  } else if (!name) {
-    const [{ intentions = [] }] = {}
-    const intention = intentions.find(
-      ({ accountid }) => accountid === hexValue
-    ) || { name: '' }
-    name = intention.name
-  }
-
+export default function VilidatorLink({ value, name, style, className }) {
+  if (!value)
+    return (
+      <Empty
+        image={Empty.PRESENTED_IMAGE_SIMPLE}
+        imageStyle={{
+          height: 16,
+          margin: 0
+        }}
+        description={false}
+      />
+    )
   return (
     <NavLink
-      to={`/accounts/${hexValue || value}`}
+      to={`/accounts/${value}`}
       style={style}
       className={classnames('nav-link', className)}
     >
