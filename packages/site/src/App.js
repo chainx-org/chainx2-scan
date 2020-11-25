a00ca3d9dbfb7b8f3c4781c2a7e37470daf9fcfe
bing <tarvos21@gmail.com>
2020-11-25 17:26:18 +0800

    Add Google Analytics to explorer

--- packages/site/src/App.js
+++ packages/site/src/App.js
@@ -9,6 +9,8 @@ import {
   fetchNativeAssetInfo
 } from '@src/store/reducers/assetSlice'
 import { localeSelector } from '@src/store/reducers/settingsSlice'
+import ReactGA from 'react-ga'
+import { createBrowserHistory } from 'history'
 
 function App() {
   const dispatch = useDispatch()
@@ -19,8 +21,17 @@ function App() {
     dispatch(fetchForeignAssetsInfo())
   }, [dispatch, locale])
 
+  const history = createBrowserHistory()
+
+  // Initialize google analytics page view tracking
+  history.listen(location => {
+    ReactGA.initialize('G-CSRS4R2T7N')
+    ReactGA.set({ page: location.pathname }) // Update the user's current page
+    ReactGA.pageview(location.pathname) // Record a pageview for the given page
+  })
+
   return (
-    <Router>
+    <Router history={history}>
       <React.Fragment>
         <Header />
         <Main />
