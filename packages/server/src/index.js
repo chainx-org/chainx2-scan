361657768de0746bf6b8a5e3ce38493e75d314fc
bing <tarvos21@gmail.com>
2020-12-02 11:02:46 +0800

    Add rate limit to koa server

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -7,8 +7,35 @@ const cors = require('@koa/cors')
 const { initDb } = require('./services/mongo')
 const config = require('../config')
 const Socket = require('socket.io')
+const ratelimit = require('koa-ratelimit')
 
 const app = new Koa()
+
+// apply rate limit
+const rateLimit = new Map()
+
+app.use(
+  ratelimit({
+    driver: 'memory',
+    db: rateLimit,
+    duration: 60000,
+    errorMessage: 'Sometimes You Just Have to Slow Down.',
+    id: ctx => ctx.ip,
+    headers: {
+      remaining: 'Rate-Limit-Remaining',
+      reset: 'Rate-Limit-Reset',
+      total: 'Rate-Limit-Total'
+    },
+    max: 100,
+    disableHeader: false,
+    whitelist: ctx => {
+      // some logic that returns a boolean
+    },
+    blacklist: ctx => {
+      // some logic that returns a boolean
+    }
+  })
+)
 
 app
   .use(logger())
