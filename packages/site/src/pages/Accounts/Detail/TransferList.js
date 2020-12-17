93f20b90b0bf151d3c986bd6efc66831d3547130
bing <tarvos21@gmail.com>
2020-12-17 15:07:10 +0800

    Change direction field location

--- packages/site/src/pages/Accounts/Detail/TransferList.js
+++ packages/site/src/pages/Accounts/Detail/TransferList.js
@@ -23,102 +23,119 @@ export default function({ address }) {
   }, [address, page, pageSize, dispatch])
 
   const { items: transfers = [], total = 0 } =
-  useSelector(transfersSelector) || {}
+    useSelector(transfersSelector) || {}
   console.log(transfers)
   const width = document.documentElement.clientWidth
   const simple = width < 1024
   return (
-      <Table
-          loading={loading}
-          onChange={({ current, pageSize: size }) => {
-            setPage(current)
-            setPageSize(size)
-          }}
-          pagination={{ current: page, pageSize, total, simple }}
-          scroll={{
-            x: '100vh'
-          }}
-          dataSource={transfers.map(item => {
-            return {
-              key: item.extrinsicHash,
-              token: 'PCX',
-              hash: (
-                  <TxLink
-                      style={{ width: 136 }}
-                      className="text-truncate"
-                      value={item.extrinsicHash}
-                  />
-              ),
-              blockHeight: <BlockLink value={item.indexer.blockHeight} />,
-              blockTime: <DateShow value={item.indexer.blockTime} />,
-              sender:
-                  item.data[0] === address ? (
-                      <div style={{ width: 138 }} className="text-truncate">
-                        {address}
-                      </div>
-                  ) : (
-                      <AccountLink
-                          style={{ width: 136 }}
-                          className="text-truncate"
-                          value={item.data[0]}
-                      />
-                  ),
-              receiver:
-                  item.data[1] === address ? (
-                      <div style={{ width: 138 }} className="text-truncate">
-                        {address}
-                      </div>
-                  ) : (
-                      <AccountLink
-                          style={{ width: 136 }}
-                          className="text-truncate"
-                          value={item.data[1]}
-                      />
-                  ),
-              value: <Amount value={item.data[2]} symbol={item.token} />,
-              direction: item.data[0] === address ? (<div style={{
-                    display:'flex',
-                    justifyContent:'center',background:'#D32F2F',color:'white'}}>
-                    {$t('transfer_out')}</div>) :
-                  (<div style={{
-                    display:'flex',
-                    justifyContent:'center',background:'#4CAF50',color:'white'}}>{$t('transfer_in')}</div>)
-            }
-          })}
-          columns={[
-            {
-              title: $t('ASSETNAME'),
-              dataIndex: 'token'
-            },
-            {
-              title: $t('block_height'),
-              dataIndex: 'blockHeight'
-            },
-            {
-              title: $t('block_time'),
-              dataIndex: 'blockTime'
-            },
-            {
-              title: $t('ex_hash'),
-              dataIndex: 'hash'
-            },
-            {
-              title: $t('sender'),
-              dataIndex: 'sender'
-            },
-            {
-              title: $t('receiver'),
-              dataIndex: 'receiver'
-            },
-            {
-              title: $t('transfer_balance'),
-              dataIndex: 'value'
-            },
-            {
-              title: $t('dex_order_direction'),
-              dataIndex: 'direction'
-            }
-          ]}
-      />
+    <Table
+      loading={loading}
+      onChange={({ current, pageSize: size }) => {
+        setPage(current)
+        setPageSize(size)
+      }}
+      pagination={{ current: page, pageSize, total, simple }}
+      scroll={{
+        x: '100vh'
+      }}
+      dataSource={transfers.map(item => {
+        return {
+          key: item.extrinsicHash,
+          token: 'PCX',
+          hash: (
+            <TxLink
+              style={{ width: 136 }}
+              className="text-truncate"
+              value={item.extrinsicHash}
+            />
+          ),
+          blockHeight: <BlockLink value={item.indexer.blockHeight} />,
+          blockTime: <DateShow value={item.indexer.blockTime} />,
+          sender:
+            item.data[0] === address ? (
+              <div style={{ width: 138 }} className="text-truncate">
+                {address}
+              </div>
+            ) : (
+              <AccountLink
+                style={{ width: 136 }}
+                className="text-truncate"
+                value={item.data[0]}
+              />
+            ),
+          receiver:
+            item.data[1] === address ? (
+              <div style={{ width: 138 }} className="text-truncate">
+                {address}
+              </div>
+            ) : (
+              <AccountLink
+                style={{ width: 136 }}
+                className="text-truncate"
+                value={item.data[1]}
+              />
+            ),
+          value: <Amount value={item.data[2]} symbol={item.token} />,
+          direction:
+            item.data[0] === address ? (
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  background: '#D32F2F',
+                  color: 'white'
+                }}
+              >
+                {$t('transfer_out')}
+              </div>
+            ) : (
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  background: '#4CAF50',
+                  color: 'white'
+                }}
+              >
+                {$t('transfer_in')}
+              </div>
+            )
+        }
+      })}
+      columns={[
+        {
+          title: $t('ASSETNAME'),
+          dataIndex: 'token'
+        },
+        {
+          title: $t('block_height'),
+          dataIndex: 'blockHeight'
+        },
+        {
+          title: $t('block_time'),
+          dataIndex: 'blockTime'
+        },
+        {
+          title: $t('ex_hash'),
+          dataIndex: 'hash'
+        },
+        {
+          title: $t('dex_order_direction'),
+          dataIndex: 'direction'
+        },
+        {
+          title: $t('sender'),
+          dataIndex: 'sender'
+        },
+        {
+          title: $t('receiver'),
+          dataIndex: 'receiver'
+        },
+        {
+          title: $t('transfer_balance'),
+          dataIndex: 'value'
+        }
+      ]}
+    />
   )
 }
