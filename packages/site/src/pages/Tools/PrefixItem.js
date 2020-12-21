ce47969eab168b0b7786b7c94d73fd6e9ff625c9
limiu331 <845217637@qq.com>
2020-12-21 14:44:54 +0800

    add PrefixItem component

--- packages/site/src/pages/Tools/PrefixItem.js
+++ packages/site/src/pages/Tools/PrefixItem.js
@@ -1,22 +1,15 @@
-import React from 'react';
-import hash from "../../assets/hash.svg";
-export default function PrefixItem(props){
-    return (
-        <li style={{
-            width: '566px',
-            height: '90px',
-            background: '#f8f9fa',
-            marginTop: '40px',
-            display: 'flex',
-            alignItems: 'center'
-        }}>
-                            <span style={{marginLeft: '18px'}}>
-                                <img src={hash} style={{width: '32px', height: '32px'}}/>
-                            </span>
-            <div style={{marginLeft: '16px'}}>
-                <div style={{fontWeight: 'bold'}}>{props.prefix}</div>
-                <div>{props.value}</div>
-            </div>
-        </li>
-    )
+import React from 'react'
+import hash from '../../assets/hash.svg'
+export default function PrefixItem(props) {
+  return (
+    <li style={props.style}>
+      <span style={{ marginLeft: '18px' }}>
+        <img src={hash} style={{ width: '32px', height: '32px' }} />
+      </span>
+      <div style={{ marginLeft: '16px' }}>
+        <div style={{ fontWeight: 'bold' }}>{props.prefix}</div>
+        <div>{props.value}</div>
+      </div>
+    </li>
+  )
 }
